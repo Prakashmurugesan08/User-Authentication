37fe984f529d8e200bfa26494c706c6705f1b962
prakash <prakashkuttycj7@gmail.com>
2022-08-28 12:26:53 +0530

    User Authentication

---
+++ UserAuthentication/src/main/java/com/simplilearn/demo/AuthenticationUser.java
@@ -0,0 +1,16 @@
+package com.simplilearn.demo;
+
+public class AuthenticationUser {
+	public String username()
+	{
+		String email = "prakash@gmail.com";
+		return email;
+	}
+	
+	public String paswd()
+	{
+		String password = "123";
+		return password;
+	}
+
+}
